f004374e8b6f24ab8c7fc0748d581b9985d57163
Shreyash Saripalli <75160891+shreyash-s@users.noreply.github.com>
2022-03-26 23:55:50 +0530

    Add files via upload

---
+++ arithmeticmean.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n; cin.ignore();
+    int num[n];
+    for (int i = 0; i < n; i++) {
+        
+        cin >> num[i]; cin.ignore();
+    }
+      
+    double am;
+    double sum =0;
+    for(int j=0;j<n;j++){
+        sum = sum + num[j];
+    }
+     am = sum/n;
+     
+    cout<<am<<endl;
+
+
+  
+}
